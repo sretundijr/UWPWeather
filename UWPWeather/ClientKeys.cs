243bfd215fcc21c931b70e5c9c1dfe80007d756d
sretundijr <sretundijr@gmail.com>
2016-06-23 15:48:52 -0600

    Add project files.

---
+++ UWPWeather/ClientKeys.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UWPWeather
+{
+    public class ClientKeys
+    {
+    }
+
+    public partial class OpenWeatherMapProxy
+    {
+        private const string WeatherApiKey = "Enter api key here";
+    }
+}

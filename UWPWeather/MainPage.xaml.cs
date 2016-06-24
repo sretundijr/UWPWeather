56243c07bce158362920260867bb5fb72cc87b81
sretundijr <sretundijr@gmail.com>
2016-06-24 00:12:21 -0600

    added tile

--- UWPWeather/MainPage.xaml.cs
+++ UWPWeather/MainPage.xaml.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI.Notifications;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -32,11 +33,25 @@ namespace UWPWeather
         {
             var position = await LocationManager.GetPosition();
 
-            //RootObject myWeather = await OpenWeatherMapProxy
-            //    .GetWeather(position.Coordinate.Latitude, position.Coordinate.Longitude);
+            var lat = position.Coordinate.Latitude;
+            var lon = position.Coordinate.Longitude;
 
             RootObject myWeather = await OpenWeatherMapProxy
-                .GetWeather();
+                .GetWeather(lat, lon);
+
+            //RootObject myWeather = await OpenWeatherMapProxy
+            //    .GetWeather();
+
+
+            //Schedule update
+            var uri = string.Format("http://uwpweatherappservice20160623104638.azurewebsites.net/?lat={0}&lon={1}", lat, lon);
+
+            var tileContent = new Uri(uri);
+            var requestedInterval = PeriodicUpdateRecurrence.HalfHour;
+
+            var updater = TileUpdateManager.CreateTileUpdaterForApplication();
+            updater.StartPeriodicUpdate(tileContent, requestedInterval);
+
 
             string icon = String.Format("ms-appx:///Assets/Weather/{0}.png", myWeather.weather[0].icon);
             WeatherResultIcon.Source = new BitmapImage(new Uri(icon, UriKind.Absolute));
